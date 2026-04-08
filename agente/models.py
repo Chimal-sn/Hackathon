1b47db3f98b3ec8b23ebc05e0341f257bea912e9
Sony <sonychimo10@hotmail.com>
2026-04-08 10:54:29 -0600

    Add. Modelo Cliente

--- agente/models.py
+++ agente/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=200)
+    nps = models.IntegerField()
+    num_quejas = models.IntegerField(default=0)
+    comentario = models.TextField()
+    riesgo_score = models.DecimalField(max_digits=5, decimal_places=2)
+    en_riesgo = models.BooleanField(default=False)
+    analisis_ia = models.TextField(blank=True, null=True)
+    estrategia_ia = models.TextField(blank=True, null=True)    
+    
+    
+    

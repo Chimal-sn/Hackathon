e89b3dbd30a094f5c18d5f406d201ac638af6ba8
Sony <sonychimo10@hotmail.com>
2026-04-08 20:54:18 -0600

    Add. Formulario de agregar clientes y su guardado con la base de datos

--- agente/views.py
+++ agente/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 # Create your views here.
@@ -6,12 +6,25 @@ def home(request):
     return HttpResponse("<h1>¡Agente funcionando!</h1>")
 
 from .models import Cliente
+from .forms import ClienteForm
 
 def clientes(request):
+    # Si la página recibe datos de un formulario (POST)
+    if request.method == 'POST':
+        form = ClienteForm(request.POST) 
+        if form.is_valid():
+            form.save() # ¡Guardar en BD!
+            return redirect('clientes') # Recargar la página limpia
+    else:
+        # Si la carga normal, crear un formulario vacío
+        form = ClienteForm()
+
+    # Traer la lista actualizada de clientes
     lista_clientes = Cliente.objects.all()
     
     contexto = {
-        'clientes': lista_clientes
+        'clientes': lista_clientes,
+        'form': form
     }
     
     return render(request, 'clientes.html', contexto)

17b8f75cf37850fd5fcc69693b911dddc3822127
Sony <sonychimo10@hotmail.com>
2026-04-09 08:26:02 -0600

    Variable de base de datos

--- core/settings.py
+++ core/settings.py
@@ -76,14 +76,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'core.wsgi.application'
 
 
-# Database
-# Usar dj_database_url para conectarse automáticamente a PostreSQL en Railway si existe
-import dj_database_url
 DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
